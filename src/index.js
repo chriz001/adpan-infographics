16aa2e685b7d029c8a0a0c63ffa413302b010731
Chris Rowe <chriz001@gmail.com>
2020-04-18 20:42:37 +1000

    map data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//import 'resize-observer-polyfill';
+//import 'core-js/modules/es.object.entries';
+
 import Timeline from "./Timeline";
 import timelineData from "../data/timeline";
 
 const timelineElement = document.getElementById("timeline");
-ReactDOM.render(<Timeline items={timelineData} />, timelineElement);
+if (timelineElement) {
+  ReactDOM.render(<Timeline items={timelineData} />, timelineElement);
+}
